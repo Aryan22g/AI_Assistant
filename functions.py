c78f3e2986e0cb3ad65fde90b67181c2412cf1d1
Aryan <54545147+Aryan22g@users.noreply.github.com>
2025-05-25 14:20:51 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def open_application(application_name):
+    if application_name and application_name.strip():
+        try:
+            subprocess.Popen(application_name)
+        except FileNotFoundError:
+            print(f"Application '{application_name}' not found. Make sure it's in your PATH.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    else:
+        print("Invalid application name.")
+
+#open_application("chrome")
+#open_application("notepad")
