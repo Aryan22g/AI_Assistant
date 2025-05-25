80219c2f32e933b308ea55b8b3c27d1314a718b9
Aryan <54545147+Aryan22g@users.noreply.github.com>
2025-05-25 14:33:58 +0530

    Update functions.py
    NSWorkspace, works in MAC

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 import subprocess
+from AppKit import NSWorkspace
 
 def open_application(application_name):
     if application_name and application_name.strip():
         try:
-            subprocess.Popen(application_name)
+            # subprocess.Popen(application_name)
+            NSWorkspace.sharedWorkspace().launchApplication_(application_name)
         except FileNotFoundError:
             print(f"Application '{application_name}' not found. Make sure it's in your PATH.")
         except Exception as e:
@@ -11,5 +13,5 @@ def open_application(application_name):
     else:
         print("Invalid application name.")
 
-#open_application("chrome")
+open_application("Sublime Text")
 #open_application("notepad")
